4f22b53a9e1ab8b1ad9a9c5e50d3b4eedc15a5ed
Anouar DERDOURI <anouar.derdouri.development@gmail.com>
2023-04-30 11:49:32 +0000

    NumericUpDownFragment: Implement logic

--- app/src/main/java/com/anouarderdouri/m213_ch08_cours/NumericUpDownFragment.java
+++ app/src/main/java/com/anouarderdouri/m213_ch08_cours/NumericUpDownFragment.java
@@ -2,13 +2,24 @@ package com.anouarderdouri.m213_ch08_cours;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class NumericUpDownFragment extends Fragment {
+    Button btnDecrement, btnIncrement;
+    TextView tvValue;
+
+    int min = 0;
+    int max = 9;
+    int value = min;
+
     public NumericUpDownFragment() {
         // Required empty public constructor
     }
@@ -19,4 +30,30 @@ public class NumericUpDownFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_numeric_up_down, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnDecrement = view.findViewById(R.id.btnDecrement);
+        btnIncrement = view.findViewById(R.id.btnIncrement);
+        tvValue = view.findViewById(R.id.tvValue);
+
+        tvValue.setText(Integer.toString(value));
+
+        btnDecrement.setOnClickListener(v -> {
+            if (value > min) {
+                value--;
+                tvValue.setText(Integer.toString(value));
+            }
+        });
+
+        btnIncrement.setOnClickListener(v -> {
+            if (value < max) {
+                value++;
+                tvValue.setText(Integer.toString(value));
+            }
+        });
+    }
+
 }
